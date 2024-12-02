dc696ef0232430552307d838e4ab96e45469230c
Sirvey <hry.oween@gmail.com>
2024-12-02 17:56:16 +0100

    Fragen - Business

--- src/types.ts
+++ src/types.ts
@@ -31,12 +31,12 @@ export interface InterviewState {
 export const questions = [
   "Können Sie uns kurz Ihr Unternehmen und dessen Zielsetzung vorstellen?",
   "Was hat Sie persönlich dazu motiviert, ein Social Business zu gründen?",
-  "Welche spezifischen Herausforderungen haben Sie bei der Gründung und im laufenden Betrieb Ihrer Organisation erlebt?",
+  "Welche spezifischen Herausforderungen haben Sie bei der Gründung und im laufenden Betrieb Ihrers Business erlebt?",
   "Inwiefern spielt Nachhaltigkeit (sozial, ökologisch und ökonomisch) eine Rolle für Ihren Erfolg?",
   "Wie gehen Sie mit Zielkonflikten zwischen ökonomischen und sozialen Zielen um?",
   "Welche Rolle spielen alternative Finanzierungsquellen (z. B. Spenden, Crowdfunding, Impact-Investments) in Ihrem Geschäftsmodell?",
-  "Wie messen Sie den sozialen oder ökologischen Impact Ihres Unternehmens? Welche Methoden verwenden Sie?",
-  "Welche drei Hauptfaktoren würden Sie als entscheidend für den Erfolg Ihres Unternehmens bezeichnen?",
+  "Wie messen Sie den sozialen oder ökologischen Impact Ihres Business? Welche Methoden verwenden Sie?",
+  "Welche drei Hauptfaktoren würden Sie als entscheidend für den Erfolg Ihres Business bezeichnen?",
   "Wie sehen Sie die Zukunft von Social Entrepreneurship in Deutschland?",
   "Welche Ratschläge würden Sie zukünftigen Gründern im Bereich Social Entrepreneurship geben, basierend auf Ihren Erfahrungen?"
 ];
